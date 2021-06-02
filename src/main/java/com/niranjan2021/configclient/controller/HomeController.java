1b218250f6b8af08ce0e357150b51b1f4b3bb144
niranjan-l <raoniranjan007@gmail.com>
2021-06-02 21:24:08 +0530

    keystore

--- src/main/java/com/niranjan2021/configclient/controller/HomeController.java
+++ src/main/java/com/niranjan2021/configclient/controller/HomeController.java
@@ -4,17 +4,27 @@ package com.niranjan2021.configclient.controller;
 import com.niranjan2021.configclient.LoadConfigProperties;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.ws.rs.DefaultValue;
+
 @RestController
+@Configuration
+@RefreshScope
 public class HomeController {
 
     @Autowired
     private LoadConfigProperties loadConfigProperties;
 
+
     @Value("${some.other.property}")
     private String someOtherProperty;
+
+    @Value("${name:king}")
+    private  String name;
 
     @GetMapping
     public  String getConfigPropertiesFromConfigServer(){
@@ -24,6 +34,14 @@ public class HomeController {
         response.append("||");
         response.append(loadConfigProperties.getProperty());
         return response.toString();
+    }
+
+
+
+    @GetMapping("/testpropertiesload")
+    public  String getPropertiesFromApp(){
+
+        return name;
     }
 
 }
